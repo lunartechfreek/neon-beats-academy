f607082236fa891d0d1dc8c2d3c39e4e5b34bc79
Daniel Turner <lunartechfreek@googlemail.com>
2024-10-30 23:55:51 +0000

    Customise admin for Course and CourseTier models

--- courses/admin.py
+++ courses/admin.py
@@ -2,7 +2,22 @@ from django.contrib import admin
 from .models import Course, CourseTier
 
 class CourseAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'tier',
+        'price',
+        'image',
+    )
+
+    ordering = ('sku',)
     exclude = ('sku',)  # Exclude SKU from the admin form
 
-admin.site.register(Course)
-admin.site.register(CourseTier)
+class CourseTierAdmin(admin.ModelAdmin):
+    list_display = (
+        'tier_name',
+        'has_description'
+    )
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(CourseTier, CourseTierAdmin)
