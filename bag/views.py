7cba4a1b835ffc61e72cd3905e78c37ae357e7b1
Daniel Turner <lunartechfreek@googlemail.com>
2024-10-31 22:50:47 +0000

    Add toasts functionality and styles

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
+
+from courses.models import Course
 
 def view_bag(request):
     """ A view that renders the bag contents page """
@@ -9,6 +12,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add the specified course to the shopping bag """
 
+    course = Course.objects.get(pk=item_id)
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
@@ -16,10 +20,10 @@ def add_to_bag(request, item_id):
     bag[item_id] = 1  # Only add one instance of each course
 
     request.session['bag'] = bag
+    messages.success(request, f'Added {course.name} to your bag')
+
     return redirect(redirect_url)
 
-
-from django.http import HttpResponse
 
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
