2206e5098c600f398fef2bea164bda7bb97baad8
Daniel Turner <lunartechfreek@googlemail.com>
2024-10-31 12:39:44 +0000

    Add course detail url, view and template

--- courses/views.py
+++ courses/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Course
 
 def all_courses(request):
-    """ A view to show all products, including sorting and search queries """
+    """ A view to show all courses, including sorting and search queries """
 
     courses = Course.objects.all()
 
@@ -11,3 +11,15 @@ def all_courses(request):
     }
 
     return render(request, 'courses/courses.html', context)
+
+
+def course_detail(request, course_id):
+    """ A view to show individual course details """
+
+    course = get_object_or_404(Course, pk=course_id)
+
+    context = {
+        'course': course,
+    }
+
+    return render(request, 'courses/course_detail.html', context)
