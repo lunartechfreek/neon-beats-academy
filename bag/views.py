6fd49efcea5510e4aa6b75e33d650fc6bfcfb071
Daniel Turner <lunartechfreek@googlemail.com>
2024-10-31 23:42:42 +0000

    Add success messages to views

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from courses.models import Course
@@ -12,7 +12,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add the specified course to the shopping bag """
 
-    course = Course.objects.get(pk=item_id)
+    course = get_object_or_404(Course, pk=item_id)
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
@@ -29,14 +29,17 @@ def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
 
     try:
+        course = get_object_or_404(Course, pk=item_id)
         bag = request.session.get('bag', {})
         
         # Remove the item directly
         if item_id in bag:
             bag.pop(item_id)
+            messages.success(request, f'Removed {course.name} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
