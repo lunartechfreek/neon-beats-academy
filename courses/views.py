18465e9a9a6e7921458b2386a87b416e27765ae2
Daniel Turner <lunartechfreek@googlemail.com>
2024-10-31 15:12:01 +0000

    Add tier filtering functionality

--- courses/views.py
+++ courses/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Course
+from .models import Course, CourseTier
 
 def all_courses(request):
     """ A view to show all courses, including sorting and search queries """
 
     courses = Course.objects.all()
 
+    tiers = None
+
+    if request.GET:
+        if 'tier' in request.GET:
+            tiers = request.GET['tier'].split(',')
+            courses = courses.filter(tier__tier_name__in=tiers)
+            tiers = CourseTier.objects.filter(tier_name__in=tiers)
+
     context = {
         'courses': courses,
+        'current_tiers': tiers,
     }
 
     return render(request, 'courses/courses.html', context)
