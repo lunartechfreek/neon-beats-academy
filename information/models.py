cec04dc3b7c9429e9203c5242752aa9a374fadf6
Daniel Turner <lunartechfreek@googlemail.com>
2024-11-11 00:18:45 +0000

    Add ContactUs model and migrations

--- information/models.py
+++ information/models.py
@@ -15,3 +15,19 @@ class About(models.Model):
 
     class Meta:
         verbose_name_plural = "About"
+
+
+class ContactUs(models.Model):
+    """
+    A model for the contact us form.
+    """
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Enquiry sent from {self.name}"
+
+    class Meta:
+        verbose_name_plural = "Contact Us"
