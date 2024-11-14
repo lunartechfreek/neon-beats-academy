b369e7f274460b76af8453867716561b1ea35643
Daniel Turner <lunartechfreek@googlemail.com>
2024-11-14 03:43:26 +0000

    Add functionality for custom error pages

--- neon_beats/urls.py
+++ neon_beats/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import handler404, handler500
 # from django.conf import settings
 # from django.conf.urls.static import static
 
@@ -29,3 +30,6 @@ urlpatterns = [
     path('information/', include('information.urls')),
 # ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
+
+handler404 = "ravehub.views.handler404"
+handler500 = "ravehub.views.handler500"
