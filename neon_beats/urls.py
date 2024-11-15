83da2a4c498f40ed74279f997207bc1cd66690e1
Daniel Turner <lunartechfreek@googlemail.com>
2024-11-15 00:28:42 +0000

    Configure newsletter url paths

--- neon_beats/urls.py
+++ neon_beats/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('information/', include('information.urls')),
+    path('newsletter/', include('newsletter.urls')),
 # ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
 
