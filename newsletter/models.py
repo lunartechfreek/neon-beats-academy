7f1d118a9fcc53abe74cbe18ced82c69d8577b27
Daniel Turner <lunartechfreek@googlemail.com>
2024-11-15 00:29:02 +0000

    Add model and migrations

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Newsletter(models.Model):
+    email = models.EmailField(unique=True, blank=False)
+    date_subscribed = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'Newsletter Subscriber'
+        verbose_name_plural = 'Newsletter Subscribers'
