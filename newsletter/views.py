f3f2deea40916606ac186d91ead634b5f4605a17
Daniel Turner <lunartechfreek@googlemail.com>
2024-11-15 00:30:28 +0000

    Add view and form functionality

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .models import Newsletter
+from .forms import NewsletterForm
 
-# Create your views here.
+def subscribe(request):
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            # Check if email already exists in the database
+            if Newsletter.objects.filter(email=email).exists():
+                messages.error(request, "You are already subscribed to the newsletter!")
+            else:
+                form.save()
+                messages.success(request, "Thank you for subscribing to our newsletter!")
+    else:
+        form = NewsletterForm()
+
+    return render(request, 'newsletter/subscribe.html', {'form': form})
