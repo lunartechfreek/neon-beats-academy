fca19f38af6570e2c5268195a68567d50b3bd1ff
Daniel Turner <lunartechfreek@googlemail.com>
2024-11-15 00:30:41 +0000

    Register newsletter in admin

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Newsletter
 
-# Register your models here.
+@admin.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ('email', 'date_subscribed')
+    search_fields = ('email',)
+    list_filter = ('date_subscribed',)
+    fields = ('email',)
+    readonly_fields = ('date_subscribed',)
