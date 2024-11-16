3e53d2d1f4b2b18f4ff6d31583ace83e3fb08126
Daniel Turner <lunartechfreek@googlemail.com>
2024-11-16 15:36:39 +0000

    Add NewsletterInfo models and migrations

--- newsletter/models.py
+++ newsletter/models.py
@@ -10,3 +10,15 @@ class Newsletter(models.Model):
     class Meta:
         verbose_name = 'Newsletter Subscriber'
         verbose_name_plural = 'Newsletter Subscribers'
+
+
+class NewsletterInfo(models.Model):
+    newsletter_info = models.TextField(unique=True, blank=False)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Newsletter Info (Updated on {self.updated_on:%Y-%m-%d %H:%M})"
+
+    class Meta:
+        verbose_name = "Newsletter Information"
+        verbose_name_plural = "Newsletter Information"
