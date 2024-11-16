fc231a0b9ef09878ebc28c73f54fcb40604ecd1a
Daniel Turner <lunartechfreek@googlemail.com>
2024-11-16 15:36:53 +0000

    Register NewsletterInfo in admin panel

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Newsletter
+from .models import Newsletter, NewsletterInfo
 
 @admin.register(Newsletter)
 class NewsletterAdmin(admin.ModelAdmin):
@@ -8,3 +8,9 @@ class NewsletterAdmin(admin.ModelAdmin):
     list_filter = ('date_subscribed',)
     fields = ('email',)
     readonly_fields = ('date_subscribed',)
+
+
+@admin.register(NewsletterInfo)
+class NewsletterInfoAdmin(admin.ModelAdmin):
+    list_display = ('newsletter_info', 'updated_on')
+    readonly_fields = ('updated_on',)
