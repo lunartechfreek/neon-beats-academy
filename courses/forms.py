8cdeb23725365fda327c2d15b103756eb6769f6d
Daniel Turner <lunartechfreek@googlemail.com>
2024-11-18 18:54:47 +0000

    Bug Fix: Add Summernote for add/updating courses

--- courses/forms.py
+++ courses/forms.py
@@ -1,13 +1,15 @@
 from django import forms
+from django_summernote.widgets import SummernoteWidget
 from .widgets import CustomClearableFileInput
 from .models import Course, CourseTier
 
-
 class CourseForm(forms.ModelForm):
-
     class Meta:
         model = Course
         fields = '__all__'
+        widgets = {
+            'description': SummernoteWidget(),  # Use Summernote for the description field
+        }
 
     image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
