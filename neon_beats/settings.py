7f17b123dc8a3296f2ef6f763b630cea05cb837a
Daniel Turner <lunartechfreek@googlemail.com>
2024-11-19 15:11:47 +0000

    Bug Fix: Summernote editor display

--- neon_beats/settings.py
+++ neon_beats/settings.py
@@ -214,3 +214,11 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+#Summernote
+SUMMERNOTE_CONFIG = {
+    'summernote': {
+        # Change editor size
+        'width': '100%',
+    },
+}
