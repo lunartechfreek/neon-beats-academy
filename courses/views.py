b6776e89b304be126ba4b6b1b1d0e5a9ab031049
Daniel Turner <lunartechfreek@googlemail.com>
2024-11-23 03:09:05 +0000

    Remove commented out code

--- courses/views.py
+++ courses/views.py
@@ -29,17 +29,6 @@ def all_courses(request):
     return render(request, 'courses/courses.html', context)
 
 
-# def course_detail(request, course_id):
-#     """ A view to show individual course details """
-
-#     course = get_object_or_404(Course, pk=course_id)
-
-#     context = {
-#         'course': course,
-#     }
-
-#     return render(request, 'courses/course_detail.html', context)
-
 def course_detail(request, course_id):
     """ A view to show individual course details """
 
